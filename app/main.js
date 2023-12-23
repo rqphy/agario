3379a7bb007076d8d07f1a9cd609a7eb8a1a0eed
rqphy <raphael.ferreira@hetic.net>
2023-12-23 23:24:31 +0100

    :sparkles: init pixi canvas

--- app/main.js
+++ app/main.js
@@ -1,1 +1,11 @@
 import "./styles/main.css"
+import * as PIXI from "pixi.js"
+
+const APP = document.querySelector("#app")
+
+const game = new PIXI.Application({
+	background: "#1099bb",
+	resizeTo: window,
+})
+
+app.appendChild(game.view)
