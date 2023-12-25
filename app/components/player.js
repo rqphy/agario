76b30cf6b4c4e4bc26bf8ab219190222659668bc
rqphy <raphael.ferreira@hetic.net>
2023-12-25 22:14:56 +0100

    :construction: create orb component

--- app/components/player.js
+++ app/components/player.js
@@ -5,12 +5,17 @@ export default class Player {
 	constructor(game) {
 		this.game = game
 		this.mesh = new PIXI.Graphics()
+		this.size = 50
 		this.draw()
 	}
 
 	draw() {
 		this.mesh.beginFill(0x00ffff, 1)
-		this.mesh.drawCircle(window.innerWidth / 2, window.innerHeight / 2, 50)
+		this.mesh.drawCircle(
+			window.innerWidth / 2,
+			window.innerHeight / 2,
+			this.size
+		)
 		this.mesh.endFill()
 		this.game.stage.addChild(this.mesh)
 	}
