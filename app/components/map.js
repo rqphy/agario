bb6ad963f77a18c180918e78e79628374c7748da
rqphy <raphael.ferreira@hetic.net>
2024-01-13 17:42:50 +0100

    :sparkles: update map size with limits

--- app/components/map.js
+++ app/components/map.js
@@ -4,5 +4,7 @@ import * as PIXI from "pixi.js"
 export default class Map {
 	constructor() {
 		this.mesh = new PIXI.Container()
+		this.mesh.height = general.limits.y
+		this.mesh.width = general.limits.x
 	}
 }
