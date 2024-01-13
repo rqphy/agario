b3dbb42d0deec9ccc2ae6819308c247d26269d51
rqphy <raphael.ferreira@hetic.net>
2024-01-13 17:51:44 +0100

    :memo: todo fix map movement

--- app/components/map.js
+++ app/components/map.js
@@ -8,16 +8,24 @@ export default class Map {
 		this.mesh = new PIXI.Container()
 		this.mesh.height = general.limits.y
 		this.mesh.width = general.limits.x
-		this.targetPosition = { x: 0, y: 0 }
+		this.targetPosition = {
+			x: general.limits.x * 0.5,
+			y: general.limits.y * 0.5,
+		}
 	}
 
 	update() {
 		// update mesh position
-		console.log(this.mesh.position)
+		// to fix : issue between cursor position (relative to window size) and map size
+		console.log(this.mesh.position.x, this.targetPosition.x)
 		const newPosition = {
-			x: lerp(this.mesh.position.x, this.targetPosition.x, 0.004),
-			y: lerp(this.mesh.position.y, this.targetPosition.y, 0.004),
+			x: this.targetPosition.x,
+			y: this.targetPosition.y,
 		}
+		// const newPosition = {
+		// 	x: lerp(this.mesh.position.x, this.targetPosition.x, 0.004),
+		// 	y: lerp(this.mesh.position.y, this.targetPosition.y, 0.004),
+		// }
 
 		this.mesh.position.copyFrom(newPosition)
 	}
